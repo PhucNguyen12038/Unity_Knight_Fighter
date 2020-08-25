f5d7f0f963dc9283786e561ebcc37da58ae18d3d
Nguyen Phuc <nguyenphuc@Nguyens-MacBook-Pro.local>
2020-08-25 22:14:57 +0300

    Knight_AI_VS_Human

--- Knight_Fighter/Assets/Scripts/AttackDamage.cs
+++ Knight_Fighter/Assets/Scripts/AttackDamage.cs
@@ -8,18 +8,15 @@ public class AttackDamage : MonoBehaviour
     public float radius = 1f;
     public float damage = 1f;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         Collider[] hits = Physics.OverlapSphere(transform.position, radius, layer);
         if (hits.Length > 0)
         {
-
+            hits[0].GetComponent<HealthScript>().ApplyDamage(damage);
+            gameObject.SetActive(false);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
---
+++ Knight_Fighter/Assets/Scripts/CharacterSoundFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundFX : MonoBehaviour
+{
+    private AudioSource soundFX;
+
+    [SerializeField]
+    private AudioClip attack_Sound, die_Sound;
+
+    void Awake()
+    {
+        soundFX = GetComponent<AudioSource>();
+    }
+
+    public void Attack()
+    {
+        soundFX.clip = attack_Sound;
+        soundFX.Play();
+    }
+
+    public void Die()
+    {
+        soundFX.clip = die_Sound;
+        soundFX.Play();
+    }
+    
+    
+}
